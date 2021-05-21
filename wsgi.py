2a9110218cd7423ca18523cedb4fd1b7132f8505
JakobGarfalk <78230754+JakobGarfalk@users.noreply.github.com>
2021-05-21 12:43:21 +0200

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -14,10 +14,51 @@
 
 # --wsgi.py------------- 
 #   
-from BLOG import Kontroll
 
-if __name__=="__main__":
-    Kontroll.run()
+from BLOG import create_app
+import pstats
+import os
+
+## OPPRETT EN CONSOLL:
+# def konsoll():
+#     a_str="logs"
+#     commands={"ls":os.listdir(a_str)}
+#     inp=input("-:")
+#     get_cmd=commands.get(inp)
+#     print (get_cmd)
+#     if get_cmd !=None:
+#         add_to=input("+:")
+#         prog=str(get_cmd)+add_to
+#         try:
+#             cprog=compile(source=prog,mode='exec',filename="fil.tmp")
+#             #cprog=compile(source=add_to,mode='exec',filename="file.tmp")
+#         except:
+#             print ("Exception!")
+#         finally:
+#             exit()
+        
+# k=konsoll()
+
+###
+
+#stats = pstats.Stats('logs/GET.admin.1ms.1618581573.prof')
+#stats=pstats.Stats('logs/GET.admin.6ms.1618581573.prof')
+list_dir=os.listdir(path="logs")
+print (list_dir)
+stats=pstats.Stats('logs\POST.bruker.nybruker.134ms.1618581617.prof')
+#C:\PythonVE4git\PyVE391\logs\GET.admin.1ms.1618581573.prof
+stats.sort_stats('time', 'calls')
+stats.print_stats()
+stats.print_callers()
+_=input("press enter")
+# if __name__=="__main__":
+#     app=create_app()
+if __name__ == '__main__':
+    #from werkzeug.middleware.profiler import ProfilerMiddleware
+    ## logger de 5 tregeste funksjonene til dir logs;
+    app=create_app()
+    #app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[5], profile_dir='logs')
+    app.run(debug=False)
 # ----------------------wsgi.py EOF
 
  
